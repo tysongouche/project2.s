eee20c21e8aff3c3573530b10704b67a480eb032
tysongouche <39385160+tysongouche@users.noreply.github.com>
2021-11-14 16:35:21 -0800

    Commit 2
    Started the text section of my code.

--- project2.s
+++ project2.s
@@ -2,3 +2,7 @@
 	input: .space 1001					      #Reserve 1001 bytes for input
 	final_string: .space 4
 	error: .asciiz "Not recognized"	
+
+.text 									#text section starts
+	
+main: 									#Main Section Starts
