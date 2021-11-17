f5caa6720842a3cb571ddc6bcce7b3e2bab4637a
tysongouche <39385160+tysongouche@users.noreply.github.com>
2021-11-16 19:29:36 -0500

    Commit 34
    Repeated the previous commit but for the 3rd byte in the final string. We are converting all 4 or less bytes, so the next couple of comments will be the same

--- project2.s
+++ project2.s
@@ -146,3 +146,8 @@ lhere2:
 	slti $t3, $t7, 97					#Test for lowercase
 	slti $t4, $t7, 123					
 	add $t4, $t4, $t3
+	addi $t3, $zero, 1 
+	beq $t4, $t3, lowercase3	
+	bne $t7, $zero, modify3				#Convert byte into its true value
+here3:
+uhere3:
